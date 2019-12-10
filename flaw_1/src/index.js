9440a4417863059dc01d4db2d8b94e440da0ddbc
gravallg <guillaume.gravallon@gmail.com>
2019-12-10 22:00:07 +0100

    Basic node template, server serves html file and has a REST api

--- flaw_1/src/index.js
+++ flaw_1/src/index.js
@@ -1,4 +1,32 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
+
+// Init express.
+const app = express();
+
+// Enable CORS.
+app.use(cors());
+
+// Enable bodyParser.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set API routes
+const api = require('./api/');
+app.use('/api',api);
+
+// Static path to dist to serve the front end files.
+app.use(express.static(path.join(__dirname, '/dist')));
+
+// Set app port.
+const port = process.env.PORT || '3000';
+app.set('port', 3000);
+
+// Create HTTP server.
+const server = http.createServer(app);
+ 
+// Listen on port.
+server.listen(port, () => console.log(`Server started (localhost:${port})!`));
